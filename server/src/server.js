e2310479bc093e270a6ee99b9fe2ce21b29baa23
Arthur Rochesso <arthurochesso@gmail.com>
2023-02-28 13:32:50 +0000

    Changing from https to http

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,31 @@
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const http = require("http");
 
-require('dotenv-flow').config();
+// If you want to use https, uncomment the code below:
+// const fs = require('fs');
+// const path = require('path');
+// const https = require('https');
 
-const app = require('./app');
+require("dotenv-flow").config();
 
-const { mongoConnect } = require('./services/mongo');
-const { loadPlanetsData } = require('./models/planets.model');
-const { loadLaunchData } = require('./models/launches.model');
+const app = require("./app");
+
+const { mongoConnect } = require("./services/mongo");
+const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 
 const PORT = process.env.PORT || 8000;
 
-const server = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, '../../key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '../../cert.pem')),
-  },
-  app
-);
+// If you want to use https, uncomment the code below:
+// You will need to create the file key.pem and cert.pem as explained in the readme file.
+// const server = https.createServer(
+//   {
+//     key: fs.readFileSync(path.join(__dirname, '../../key.pem')),
+//     cert: fs.readFileSync(path.join(__dirname, '../../cert.pem')),
+//   },
+//   app
+// );
+
+const server = http.createServer(app);
 
 // Async to guarantee that the data is loaded before the server starts
 const startServer = async () => {
